167c2b3f6bff0a7fb92ef9f703a9705e92c9248b
Developer <dev@example.com>
2026-01-29 17:48:51 +0700

    Fix: Remove duplicate PORT declaration

--- server/index.js
+++ server/index.js
@@ -136,10 +136,9 @@ app.post("/api/surgery/book", upload.single("prescription"), async (req, res) =>
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
+});
 });
 
 // Start server
